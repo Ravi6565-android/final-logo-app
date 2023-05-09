ddf4f2a11363c9910899507b5e65620604f23877
ravi6565-android <ravichauhanis420@gmail.com>
2023-05-09 11:45:53 +0530

    first commit

--- app/src/main/java/com/example/logo_app/MainActivity.java
+++ app/src/main/java/com/example/logo_app/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.logo_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button trophy,play,leaderboard,statistic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void init(){
+        trophy=findViewById(R.id.main_trophy_btn);
+        statistic=findViewById(R.id.main_statistic_btn);
+        leaderboard=findViewById(R.id.main_trophy_btn);
+        play=findViewById(R.id.main_play_btn);
+
+        //on Click lisners
+        trophy.setOnClickListener(this);
+        statistic.setOnClickListener(this);
+        leaderboard.setOnClickListener(this);
+        play.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==play.getId()){
+
+
+        }
+        if(v.getId()==trophy.getId()){
+
+
+        }
+        if(v.getId()==statistic.getId()){
+
+
+        }
+        if(v.getId()==leaderboard.getId()){
+
+
+        }
     }
 }
